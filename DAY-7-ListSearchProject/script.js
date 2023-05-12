7568701b1c3ba03eb9fe28f59ee44bdec95c06d8
shreyT19 <shreyanshtripathy49@gmail.com>
2023-05-12 18:48:20 +0530

    day 7

---
+++ DAY-7-ListSearchProject/script.js
@@ -0,0 +1,23 @@
+
+
+const ul = document.querySelector('.list');
+
+
+const input = document.querySelector('.input');
+
+
+const handleChange = ()=>{
+    const inputWord = input.value;
+    const list = document.getElementsByTagName('li');
+
+
+    for(let i=0;i<list.length;i++){
+        if(list[i].innerHTML.toUpperCase().includes(inputWord.toUpperCase())){
+            list[i].style.display='';
+        }else{
+            list[i].style.display="none"
+        }
+    }
+}
+
+input.addEventListener('keyup',handleChange);
